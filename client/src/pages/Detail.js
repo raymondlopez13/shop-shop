c460d2707837767c628de8f8734c39d79ef3feb2
Raymond Lopez <raymondlopez13@yahoo.com>
2021-05-19 12:08:58 -0500

    added stripe

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,24 +1,79 @@
 import React, { useEffect, useState } from "react";
+import { useStoreContext } from '../utils/GlobalState';
 import { Link, useParams } from "react-router-dom";
 import { useQuery } from '@apollo/react-hooks';
 
 import { QUERY_PRODUCTS } from "../utils/queries";
 import spinner from '../assets/spinner.gif'
+import Cart from '../components/Cart';
+import {
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
+  ADD_TO_CART,
+  UPDATE_PRODUCTS
+} from '../utils/actions';
+import { idbPromise } from '../utils/helpers';
 
 function Detail() {
+  const [state, dispatch] = useStoreContext();
   const { id } = useParams();
 
   const [currentProduct, setCurrentProduct] = useState({})
 
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
-  const products = data?.products || [];
+  const { products, cart } = state;
+
+  const addToCart = () => {
+    const itemInCart = cart.find((cartItem) => cartItem._id === id);
+
+    if (itemInCart) {
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: id,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+      });
+      idbPromise('cart', 'put', {
+        ...itemInCart,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+      });
+    } else {
+      dispatch({
+        type: ADD_TO_CART,
+        product: { ...currentProduct, purchaseQuantity: 1}
+      });
+      idbPromise('cart', 'put', { ...currentProduct, purchaseQuantity: 1 });
+    }
+  };
+  const removeFromCart = () => {
+    dispatch({
+      type: REMOVE_FROM_CART,
+      _id: currentProduct._id
+    });
+    idbPromise('cart', 'delete', {...currentProduct});
+  };
 
   useEffect(() => {
     if (products.length) {
       setCurrentProduct(products.find(product => product._id === id));
+    } else if (data) {
+      dispatch({
+        type: UPDATE_PRODUCTS,
+        products: data.products
+      });
+
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product);
+      });
+    } else if (!loading) {
+      idbPromise('products', 'get').then((indexedProducts) => {
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: indexedProducts
+        });
+      })
     }
-  }, [products, id]);
+  }, [products, data, loading, dispatch, id]);
 
   return (
     <>
@@ -38,10 +93,13 @@ function Detail() {
             <strong>Price:</strong>
             ${currentProduct.price}
             {" "}
-            <button>
+            <button onClick={addToCart}>
               Add to Cart
             </button>
-            <button>
+            <button
+              disabled={!cart.find(p => p._id === currentProduct._id)}
+              onClick={removeFromCart}
+            >
               Remove from Cart
             </button>
           </p>
@@ -55,6 +113,7 @@ function Detail() {
       {
         loading ? <img src={spinner} alt="loading" /> : null
       }
+      <Cart />
     </>
   );
 };
--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -1,6 +1,65 @@
-export function pluralize(name, count) {
+ export function pluralize(name, count) {
   if (count === 1) {
     return name
   }
   return name + 's'
+}
+
+export function idbPromise(storeName, method, object) {
+  return new Promise((resolve, reject) => {
+    // open connection to database shop-shop with version 1
+    const request = window.indexedDB.open('shop-shop', 1);
+
+    //variables to hold reference to database
+    let db, tx, store;
+
+    //if version has changed or first time using, run to create three stores
+    request.onupgradeneeded = function(e) {
+      const db = request.result;
+      //object stores for data
+      db.createObjectStore('products', { keyPath: '_id' });
+      db.createObjectStore('categories', { keyPath: '_id' });
+      db.createObjectStore('cart', { keyPath: '_id' });
+    };
+
+    request.onerror = function(e) {
+      console.log('There was an error');
+    };
+
+    request.onsuccess = function(e) {
+      //save reference to database
+      db = request.result;
+      //open a transaction, do whatever is passed into storeName
+      tx = db.transaction(storeName, 'readwrite');
+      //save reference to that object store
+      store = tx.objectStore(storeName);
+
+      db.onerror = function(e) {
+        console.log('error', e);
+      };
+
+      switch(method) {
+        case 'put':
+          store.put(object);
+          resolve(object);
+          break;
+        case 'get':
+          const all = store.getAll();
+          all.onsuccess = function() {
+            resolve(all.result);
+          };
+          break;
+        case 'delete':
+          store.delete(object._id);
+          break;
+        default:
+          console.log('No valid Method');
+          break;
+      }
+
+      tx.oncomplete = function() {
+        db.close();
+      };
+    };
+  });
 }
